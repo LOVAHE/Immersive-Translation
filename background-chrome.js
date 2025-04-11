cb68e733ce4eed3dbc83ee2aef417cf946ea8dfa
LOVAHE <lovahe@drypeng.io>
2025-04-11 21:30:38 +0900

    Create background-chrome.js

---
+++ background-chrome.js
@@ -0,0 +1,42 @@
+# please rename to background.js for use
+
+console.log('Background script loaded');
+
+function createContextMenus() {
+  console.log('Creating context menu...');
+  
+  chrome.contextMenus.removeAll(() => {
+    chrome.contextMenus.create({
+      id: "translate-selection",
+      title: "Translate",
+      contexts: ["selection"]
+    });
+    
+    chrome.contextMenus.create({
+      id: "translate-page",
+      title: "Translate Page",
+      contexts: ["page"]
+    });
+  });
+  
+  console.log('Context menu created');
+}
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  console.log('Context menu clicked:', info.menuItemId);
+  if (info.menuItemId === "translate-selection") {
+    console.log('Sending translateSelection message...');
+    chrome.tabs.sendMessage(tab.id, {
+      action: "translateSelection",
+      text: info.selectionText
+    });
+  }
+  if (info.menuItemId === "translate-page") {
+    console.log('Sending translatePage message...');
+    chrome.tabs.sendMessage(tab.id, {
+      action: "translatePage"
+    });
+  }
+});
+
+createContextMenus();
