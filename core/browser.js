7bc6eacc041509f134111b6c2e48d6e1fd06b468
LOVAHE <lovahe@drypeng.io>
2026-04-29 17:25:31 +0900

    Swap order of chrome and browser in api export

--- core/browser.js
+++ core/browser.js
@@ -1,1 +1,18 @@
-export const api = globalThis.chrome ?? globalThis.browser;
+export const api = globalThis.browser ?? globalThis.chrome;
+
+export function callApi(fn, ...args) {
+  return new Promise((resolve, reject) => {
+    try {
+      const maybePromise = fn(...args, (...callbackArgs) => {
+        resolve(callbackArgs.length > 1 ? callbackArgs : callbackArgs[0]);
+      });
+      if (maybePromise && typeof maybePromise.then === 'function') {
+        maybePromise.then(resolve, reject);
+      } else if (fn.length <= args.length) {
+        resolve(maybePromise);
+      }
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
